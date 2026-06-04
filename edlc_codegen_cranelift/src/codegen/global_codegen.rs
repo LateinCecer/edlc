cde8de85ee7ceb4377ceb0c28ffa8d41c2ec86e1
Adrian Paskert <a_pask01@uni-muenster.de>
2026-06-04 16:14:13 +0200

    update copyright notice in files

--- edlc_codegen_cranelift/src/codegen/global_codegen.rs
+++ edlc_codegen_cranelift/src/codegen/global_codegen.rs
@@ -1,3 +1,21 @@
+/*
+ *     EDLc, a compiler for the EDL programming language.
+ *     Copyright (C) 2026  Adrian Paskert
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use cranelift_codegen::ir::InstBuilder;
 use cranelift_module::Module;
 use edlc_core::prelude::mir_expr::mir_variable::MirGlobalVar;
